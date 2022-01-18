a3b585ac3f98b68ab8e9ca9e7651981030d8e8be
nicolas-masselot <nicolasmasselot0@gmail.com>
2022-01-18 17:20:31 +0100

    backend terminé et correction de la BDD des personnes pour bien l'isoler de celle des users

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,17 @@
-﻿from flask import Flask , jsonify
+﻿from flask import Flask , request, jsonify
 from db_connect import get_connection , psycopg2
 
+SUCCESSFUL_DB_CONNECT = "Connection to the PostgreSQL established successfully."
+ERROR_DB_CONNECT = "Connection to the PostgreSQL encountered and error."
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
+@app.route('/getAllPersonnes', methods = ['POST'])
+def getAllPersonnes():
     conn = get_connection()
     if conn:
-        test_db = "Connection to the PostgreSQL established successfully."
+        test_db = SUCCESSFUL_DB_CONNECT
     else:
-        test_db = "Connection to the PostgreSQL encountered and error."
+        test_db = ERROR_DB_CONNECT
     print(test_db)
     cursor = conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
     sql = '''SELECT * FROM Personnes;'''
@@ -18,6 +20,71 @@ def hello():
     conn.close()
     return jsonify(results)
   
+@app.route('/getPersonnebyMail', methods = ['POST'])
+def getPersonneByMail():
+    mailrecherche = request.form.get("email")
+    mailrecherche = '%'+mailrecherche+'%'
+    conn = get_connection()
+    if conn:
+        test_db = SUCCESSFUL_DB_CONNECT
+    else:
+        test_db = ERROR_DB_CONNECT
+    print(test_db)
+    cursor = conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+    sql = "PREPARE getPersMail (text) AS SELECT * FROM Personnes WHERE email LIKE $1 ; EXECUTE getPersMail(%s);"
+    cursor.execute(sql,(mailrecherche,))
+    results = cursor.fetchall()
+    conn.close()
+    return jsonify(results)
+
+@app.route('/getPersonnebyPhone', methods = ['POST'])
+def getPersonneByPhone():
+    phonerecherche = request.form.get("tel")
+    conn = get_connection()
+    if conn:
+        test_db = SUCCESSFUL_DB_CONNECT
+    else:
+        test_db = ERROR_DB_CONNECT
+    print(test_db)
+    cursor = conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+    sql = "PREPARE getPersPhone (text) AS SELECT * FROM Personnes WHERE tel = $1 ; EXECUTE getPersPhone(%s);"
+    cursor.execute(sql,(phonerecherche,))
+    results = cursor.fetchall()
+    conn.close()
+    return jsonify(results)
   
+@app.route('/getPersonnebyNoms', methods = ['POST'])
+def getPersonneByNoms():
+    nomrecherche = request.form.get("nom")
+    prenomrecherche = request.form.get("prenom")
+    conn = get_connection()
+    if conn:
+        test_db = SUCCESSFUL_DB_CONNECT
+    else:
+        test_db = ERROR_DB_CONNECT
+    print(test_db)
+    cursor = conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+    if(nomrecherche == None and prenomrecherche != None):
+        prenomrecherche = '%'+prenomrecherche+'%'
+        sql = "PREPARE getPersPrenom (text) AS SELECT * FROM Personnes WHERE prenom ILIKE $1 ; EXECUTE getPersPrenom(%s);"
+        cursor.execute(sql,(prenomrecherche,))
+        results = cursor.fetchall()
+        conn.close()
+    elif(prenomrecherche == None and nomrecherche != None):
+        nomrecherche = '%'+nomrecherche+'%'
+        sql = "PREPARE getPersNom (text) AS SELECT * FROM Personnes WHERE nom ILIKE $1 ; EXECUTE getPersNom(%s);"
+        cursor.execute(sql,(nomrecherche,))
+        results = cursor.fetchall()
+        conn.close()
+    elif(prenomrecherche != None and nomrecherche != None):
+        nomrecherche = '%'+nomrecherche+'%'
+        prenomrecherche = '%'+prenomrecherche+'%'
+        sql = "PREPARE getPersNomPrenom (text,text) AS SELECT * FROM Personnes WHERE nom ILIKE $1 AND prenom ILIKE $2 ; EXECUTE getPersNomPrenom(%s,%s);"
+        cursor.execute(sql,(nomrecherche,prenomrecherche))
+        results = cursor.fetchall()
+        conn.close()
+        
+    return jsonify(results)
+
 if __name__ == "__main__":
     app.run(host ='0.0.0.0', port = 5001, debug = True) 
